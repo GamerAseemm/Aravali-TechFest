ca1d09ed16ffb8d4c3edd7a655e03b1b0a8de3d5
GamerAseemm <123302794+GamerAseemm@users.noreply.github.com>
2024-11-03 12:29:57 +0530

    pretty fied

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-  let lastScrollTop = 0;
-  const navbar = document.querySelector('.navbar');
-  let disableScrollDetection = false;
+let lastScrollTop = 0;
+const navbar = document.querySelector(".navbar");
+let disableScrollDetection = false;
 
-  window.addEventListener('scroll', function() {
-    if (disableScrollDetection) return;
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener("scroll", function () {
+  if (disableScrollDetection) return;
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-      navbar.classList.add('hidden-navbar');
-    } else {
-      navbar.classList.remove('hidden-navbar');
-    }
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add("hidden-navbar");
+  } else {
+    navbar.classList.remove("hidden-navbar");
+  }
 
-    lastScrollTop = scrollTop;
+  lastScrollTop = scrollTop;
+});
+document.querySelectorAll(".navbar a").forEach((link) => {
+  link.addEventListener("click", function () {});
+});
+
+//faq section
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
   });
-  document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', function() {
-    });
-  });
-
-  //faq section
-  const toggles = document.querySelectorAll('.faq-toggle');
-
-toggles.forEach(toggle => {
-	toggle.addEventListener('click', () => {
-		toggle.parentNode.classList.toggle('active');
-	});
 });
